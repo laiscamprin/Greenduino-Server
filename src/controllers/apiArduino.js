4ad6bde555888e20c143efb5f6bea893c450e2a6
Laís <camprinbiaelais@gmail.com>
2026-08-13 20:25:36 -0300

    código da API para receber telemetria e enviar parâmetros

--- src/controllers/apiArduino.js
+++ src/controllers/apiArduino.js
@@ -1,14 +1,60 @@
-const db = require('../config/database'); // requerimento da conexão com o banco de dados
+const connectionFactory = require('../config/connectionFactory');
+const greenduinoBanco = require('../config/greenduinoBanco')(); 
 
-const salvarTelemtria = async (request, response) => { //função assincrona para salvar dados no banco de dados
-    try {
-        const { temperatura_ar, umidade_ar, umidade_solo, bomba_acionada } = request.body;
 
-      if (temperatura_ar === undefined || umidade_ar === undefined || umidade_solo === undefined) {
-            return response.status(400).json({ 
-                error: 'Parâmetros temperatura_ar, umidade_ar e umidade_solo são obrigatórios.' 
-            });
+module.exports = {
+
+    recepcaoTelemetria: function(request, response) {
+
+        const { temperatura_ar, umidade_ar, umidade_solo, bomba_acionada } = req.body;
+
+        if (temperatura_ar === undefined || umidade_ar === undefined || umidade_solo === undefined) {
+            return res.status(400).json({ erro: 'Dados incompletos. Verifique os sensores.' });
         }
-        const statusBomba = bomba_acionada ?? 0;
+
+        const dadosInsert= {
+            temperatura_ar: temperatura_ar,
+            umidade_ar: umidade_ar,
+            umidade_solo: umidade_solo,
+            bomba_acionada: bomba_acionada || 0,
+            created_at: new Date()
+        };
+
+        const conexao = connectionFactory();
+
+        greenduinoBanco.insertTelemetria(dadosInsert, conexao, function(erro, resultado) {
+  
+            conexao.end();
+
+            if (erro) {
+                console.log('Erro ao salvar no banco:', erro);
+                return res.status(500).json({ erro: 'Erro ao salvar telemetria.' });
+            }
+
+            res.status(201).json({ mensagem: 'Telemetria gravada' });
+        });
+    }, 
+
+
+
+enviarConfiguracoes: function(request, response) {
+       
+        const conexao = connectionFactory(); 
+
+        greenduinoBanco.envioParametros(conexao, function(err, results) {
+      
+            conexao.end(); 
+            
+            if (err) {
+                console.log('Erro ao buscar configurações:', erro);
+                return response.status(500).json({ erro: 'Erro interno do servidor.' });
+            }
+
+			      if (results.length === 0) {
+                return res.status(404).json({ errr: 'Parâmetro não encontrado' });
+            }
+
+            return res.status(200).json(resultados[0]);
+        });
     }
 };
